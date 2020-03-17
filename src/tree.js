bfd11b06117a5f714a5ed0680580b2043691efaf
ericweinberg <egweinberg@viterbo.edu>
2020-03-17 14:44:37 -0500

    v1

--- src/tree.js
+++ src/tree.js
@@ -8,11 +8,13 @@ import TreeItem from '@material-ui/lab/TreeItem';
 import axios from "axios";
 
 
-const GroupedAnnotations = ({ qualifier, data }) => {
+const GroupedAnnotations = ({ qualifier, data,cnt }) => {
+    console.log("data");
+    console.log(data);
   return (
     <div>
         {Object.keys(data).map(key => (
-        <li key={key}>eee{data[key].name}</li>
+            <TreeItem nodeId={data[key].id} label={data[key].uri} />
       ))}
 
     </div>
@@ -86,49 +88,38 @@ class DyTree extends React.Component {
         console.log("Len:");
         console.log('======= Object.keys ==========');
 
-        let x=10;
         let itemList=[];
+        let x=0;
         Object.keys(this.state.data).forEach(key => {
           let value = this.state.data[key];
           console.log("here");
           console.log(value);
-           itemList.push(<div>aa{key}<GroupedAnnotations data={value} Qualifier={"aa"}/>aa</div>);
-           x++;
+            itemList.push(<TreeItem nodeId={'g-'+x.toString()} label={key}><GroupedAnnotations data={value}  Qualifier={"aa"}/></TreeItem>);
         });
-
-
-
+        console.log("array");
+        console.log(itemList);
 
         return (
 
             <div>
-            <span>{itemList}</span>
 
 
+       <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
+
+       </TreeView>
 
 
-
-           <TreeView
+            <TreeView
 
       defaultCollapseIcon={<ExpandMoreIcon />}
       defaultExpandIcon={<ChevronRightIcon />}
     >
-
-
       <TreeItem nodeId="1" label="Applications">
-        <TreeItem nodeId="2" label="Calendar" />
-        <TreeItem nodeId="3" label="Chrome" />
-        <TreeItem nodeId="4" label="Webstorm" />
+      <TreeItem nodeId="2" label="Calendar" />
+      <TreeItem nodeId="3" label="Chrome" />
+      <TreeItem nodeId="4" label="Webstorm" />
       </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="10" label="OSS" />
-        <TreeItem nodeId="6" label="Material-UI">
-          <TreeItem nodeId="7" label="src">
-            <TreeItem nodeId="8" label="index.js" />
-            <TreeItem nodeId="9" label="tree-view.js" />
-          </TreeItem>
-        </TreeItem>
-      </TreeItem>
+       {itemList}
     </TreeView>
             </div>
         );
